bf7582fc66c23eac988c6d10391b117b76bec6e6
Chojinseon <96776102+Chojinseon@users.noreply.github.com>
2022-01-05 11:16:08 +0900

    소스정리

--- FrontEnd/ai-gym-front-end/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import configureReducer from '../features/configure/configureSlice';
-
-export const store = configureStore({
-  reducer: {
-    configure: configureReducer,
-  },
-});
